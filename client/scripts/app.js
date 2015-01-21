68a8150b1b50a57239643439dc43df258f2ba704
Avi Dunn <avidunn@gmail.com>
2015-01-21 11:44:09 -0800

    Refactored chatterbox-client

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
 
   app = {};
   var time;
@@ -9,12 +8,10 @@ $(document).ready(function(){
   var fetch = function(callback){
 
     $.ajax({
-      // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=10',
       type: 'GET',
       contentType: 'application/json',
       success: function (data) {
-        // console.log(data);
         callback(data);
       },
       error: function (data) {
@@ -25,15 +22,14 @@ $(document).ready(function(){
 
   };
 
-  var post = function(message){
+  var post = function(message) {
+
     $.ajax({
-      // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function () {
-        // callback(data);
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
@@ -43,7 +39,7 @@ $(document).ready(function(){
     });
   };
 
-  var retrieveMessages = function(data){
+  var retrieveMessages = function(data) {
     $('.messageList').find('li').remove();
     $('.roomList').find('li').remove();
 
@@ -51,19 +47,19 @@ $(document).ready(function(){
     var names = [];
     var allMessages = data.results;
 
-    _.each(allMessages, function(messageObject){
+    _.each(allMessages, function(messageObject) {
 
       if (name) {
         var name = name;
-      } else if (messageObject.username){
+      } else if (messageObject.username) {
         var name = messageObject.username.replace(/<.+>/g,'')
       } else {
         var name = 'No Name Provided';
       }
 
-      if (messageObject.text){
+      if (messageObject.text) {
         var textToSend = messageObject.text.replace(/<.+>/g,'')
-      } else if (messageObject.userMessage){
+      } else if (messageObject.userMessage) {
         var textToSend = messageObject.userMessage.replace(/<.+>/g,'')
       } else {
         var textToSend = 'No Message Provided';
@@ -71,9 +67,9 @@ $(document).ready(function(){
 
       if (room) {
         var chatroom = room;
-      } else if (messageObject.roomName){
+      } else if (messageObject.roomName) {
         var chatroom = messageObject.roomName.replace(/<.+>/g,'')
-      } else if (messageObject.roomname){
+      } else if (messageObject.roomname) {
         var chatroom = messageObject.roomname.replace(/<.+>/g,'')
       } else {
         var chatroom = 'lobby';
@@ -87,12 +83,12 @@ $(document).ready(function(){
     });
 
     var uniqRooms = _.uniq(rooms);
-    _.each(uniqRooms,function(elem){
+    _.each(uniqRooms,function(elem) {
       $('.roomList').append('<li><a href="#">'+elem+'</a></li>');
     });
   }
 
-  $('button.sendMessage').on('click',function(event){
+  $('button.sendMessage').on('click',function(event) {
     var messageText = $('.messageInput').val();
     var messageRoom = $('.roomInput').val();
     var userName = window.location.search.replace(/\?username=/g, '');
@@ -104,29 +100,23 @@ $(document).ready(function(){
     post(message);
   });
 
-  $('button.refresh').on('click',function(event){
+  $('button.refresh').on('click',function(event) {
       fetch(retrieveMessages);
     });
 
-// gets roomname
   $('ul.roomList').on('click','a',function(event){
     event.preventDefault();
     var roomClass = $(this).text();
-    // room = roomClass;
     $('.messageList').find('li').hide();
     $('.messageList').find('.'+roomClass).show();
   });
 
-// gets username
   $('ul.messageList').on('click','a',function(event){
     event.preventDefault();
     var nameClass = $(this).text();
-    // name = nameClass;
     $('.messageList').find('li').hide();
     $('.messageList').find('.'+nameClass).show();
   });
 
   fetch(retrieveMessages);
-  // time = setInterval (function(){fetch(retrieveMessages)},2000);
-
 });
